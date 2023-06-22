45260e4d1c48eebdfb60303315efbf3e21f94eae
Richard Swinkels <github@richardswinkels.nl>
2023-06-22 17:48:22 +0200

    Add routes and empty views

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView';
+import StoreView from '@/views/StoreView';
+import ProductDetailView from '@/views/ProductDetailView';
+import ContactView from '@/views/ContactView';
+import NewsView from '@/views/NewsView';
+import NewsArticleView from '@/views/NewsArticleView';
+import CartView from '@/views/CartView';
+import OrderView from '@/views/OrderView';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -10,6 +17,62 @@ const router = createRouter({
             component: HomeView,
             meta: {
                 title: 'VinylVibes - Home'
+            }
+        },
+        {
+            name: 'Store',
+            path: '/store',
+            component: StoreView,
+            meta: {
+                title: 'VinylVibes - Store'
+            }
+        },
+        {
+            name: 'ProductDetail',
+            path: '/products/:id',
+            component: ProductDetailView,
+            meta: {
+                title: 'VinylVibes - Product'
+            }
+        },
+        {
+            name: 'Contact',
+            path: '/contact',
+            component: ContactView,
+            meta: {
+                title: 'VinylVibes - Contact'
+            }
+        },
+        {
+            name: 'News',
+            path: '/news',
+            component: NewsView,
+            meta: {
+                title: 'VinylVibes - News'
+            }   
+        },
+        {
+            name: 'NewsArticle',
+            path: '/news-article/:id',
+            component: NewsArticleView,
+            meta: {
+                title: 'VinylVibes - Article'
+            }
+        },
+        {
+            name: 'Cart',
+            path: '/cart',
+            component: CartView,
+            meta: {
+                title: 'VinylVibes - Cart'
+            }
+        },
+        {
+            name: 'Order',
+            path: '/order',
+            component: OrderView,
+            meta: {
+                title: 'VinylVibes - Order'
             }
         },
     ],
---
+++ src/views/NewsArticleView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NewsArticleView'
+}
+</script>
+
+<style>
+    
+</style>
