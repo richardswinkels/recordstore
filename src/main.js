5813fdd9208a2c1fd34b544554dfbe415d8be879
Richard Swinkels <github@richardswinkels.nl>
2023-06-22 23:13:49 +0200

    Install tailwind and axios

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import './styles/app.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router, VueAxios, axios).mount('#app')
