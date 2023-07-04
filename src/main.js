d330484981d8d65b9ed310aa15d4c269b8a80c26
Richard Swinkels <github@richardswinkels.nl>
2023-07-04 10:57:18 +0200

    Add google analytics

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import VueAxios from 'vue-axios';
+import VueGtag from "vue-gtag";
 import router from './router';
 import store from './store';
 import axios from 'axios';
@@ -15,4 +16,11 @@ const app = createApp(App);
 app.use(router);
 app.use(VueAxios, axios);
 app.use(store);
+app.use(VueGtag, {
+  appName: 'VinylVibes',
+  pageTrackerScreenviewEnabled: true,
+  config: {
+    id: 'G-9NQPVKBHF0',
+  },
+}, router)
 app.mount('#app');
