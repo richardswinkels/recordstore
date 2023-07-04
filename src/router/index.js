decae6b769bde5752f728bd71b738ff157521864
Richard Swinkels <github@richardswinkels.nl>
2023-07-04 21:42:27 +0200

    fix route path

--- src/router/index.js
+++ src/router/index.js
@@ -38,7 +38,7 @@ const router = createRouter({
     },
     {
       name: 'NewsArticle',
-      path: '/news-article/:id',
+      path: '/news-articles/:id',
       component: NewsArticleView,
     },
     {
